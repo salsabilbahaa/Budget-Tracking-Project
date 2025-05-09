6002ce7fa3913231e7c8f67c514cfdf3da1c0c9b
Mariem Refaey <162060948+mariemRefaey@users.noreply.github.com>
2025-05-10 02:32:11 +0300

    Delete ReminderValidator.java

--- ReminderValidator.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-
-public class ReminderValidator 
-{
-    public static boolean isValid(Reminder reminder) 
-    {
-        return reminder.getDateTime().isAfter(LocalDateTime.now());
-    }
-}
