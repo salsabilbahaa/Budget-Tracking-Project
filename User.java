8380bcb062de6a255997a97a43a8461f7616c080
Ola Ghoneim <162748731+OlaGhoneim@users.noreply.github.com>
2025-05-10 13:31:18 +0300

    Update User.java

--- User.java
+++ User.java
@@ -3,6 +3,7 @@ public class User {
     private String username;
     private String password;
     private String phoneNumber;
+    private List<BudgetingPage> budgets;
 
     public User(String email, String username, String password, String phoneNumber) {
         this.email = email;
@@ -15,5 +16,21 @@ public class User {
     public String getUsername() { return username; }
     public String getPassword() { return password; }
     public String getPhoneNumber() { return phoneNumber; }
+
+    public List<BudgetingPage> retrieveBudgets() {
+        if (budgets == null) {
+            budgets = new ArrayList<>();
+        }
+        return budgets;
+    }
+
+    public void setBudgets(List<BudgetingPage> budgets) {
+        this.budgets = budgets;
+    }
+
+    public void addBudget(BudgetingPage budget) {
+       retrieveBudgets().add(budget);
+    }
+    
 }
 
