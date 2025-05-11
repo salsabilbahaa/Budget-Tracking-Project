662583ca173d6b2a34238f754863ca735eaa9dcb
Ola Ghoneim <162748731+OlaGhoneim@users.noreply.github.com>
2025-05-12 00:04:57 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,70 +1,39 @@
 import java.util.Scanner;
 
+import java.util.*;
 public class Main {
-    private static User currentUser = null;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("**Welcome to the Budget tracking Application!**");
+        System.out.println("1. Sign Up");
+        System.out.println("2. Log in");
+        System.out.println("3. Create budget & analysis");
+        System.out.println("4. Exit");
+
+        System.out.println("____________________________");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
         while (true) {
-            System.out.println("**Welcome to the Budget tracking Application!**");
-            System.out.println("1. Sign Up");
-            System.out.println("2. Log in");
-            System.out.println("3. Create budget & analysis");
-            System.out.println("4. Income Tracking Page");
-            System.out.println("5. Reminders Page");
-            System.out.println("6. Saving and Goals Page");
-            System.out.println("7. Expense Tracking Page");
-            System.out.println("8. Debt Repayment Page");
-            System.out.println("9. Donation Page");
-            System.out.println("10. Exit");
-            System.out.println("____________________________");
-            System.out.print("Enter your choice: ");
-
-            int choice;
-            try {
-                choice = scanner.nextInt();
-                scanner.nextLine();
-            } catch (Exception e) {
-                System.out.println("Invalid input. Please enter a number.");
-                scanner.nextLine();
-                continue;
-            }
-
             switch (choice) {
                 case 1:
-                    currentUser = SignUpPage.run();
+                    SignUpPage.run();
                     break;
                 case 2:
-                    currentUser = LoginPage.run();
+                     LoginPage.run();
                     break;
                 case 3:
-                    BudgetingPage.run();
+                    BudgetMain.run();
                     break;
                 case 4:
-                    if (currentUser != null) {
-                        IncomeTrackingPage.run(currentUser);
-                    }
-                    else {
-                        System.out.println("Please login or sign up first.");
-                    }
-                    break;
-                case 5:
-                    ReminderPage.handleReminderPage();
-                case 6:
-                    SavingsGoalsPage.handleSavingsPage();
-                case 7:
-                    ExpenseTrackingPage.handleExpenseTrackingPage();
-                case 8:
-                    DebtRepaymentPage.handleDebtRepaymentPage();
-                case 9:
-                    DonationPage.handleDonationPage();
-                case 10:
                     System.out.println("Exiting...");
-                    scanner.close();
-                    return;
+                    break;
                 default:
                     System.out.println("Invalid choice.");
             }
+
+            scanner.close();
         }
     }
 }
